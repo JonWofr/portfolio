9413c1f2a1359a55f960187a986d227bbf6747dd
Jonas Wolfram <jonas-wolfram@web.de>
2022-05-31 11:37:26 +0800

    Added third testimonial

--- src/app/data/testimonials.ts
+++ src/app/data/testimonials.ts
@@ -11,6 +11,13 @@ export const testimonials: Testimonial[] = [
   {
     photoUrl: '/assets/images/testimonial-2.jpg',
     quote:
+      'The renewal of our website by Jonas has led to a verifiable rapid increase in the number of visits. Likewise, feedback from our customers confirmed that the web app is designed in a professional and user-friendly manner.',
+    name: 'Norbert Driessen',
+    jobTitle: 'CEO Thai Rin Ruam',
+  },
+  {
+    photoUrl: '/assets/images/testimonial-3.jpg',
+    quote:
       'With his know-how in web development Jonas has greatly increased my online presence. Frequent inquiries via my website and extremely positive feedback make this success easily measurable.',
     name: 'Tomke Nils',
     jobTitle: 'CEO moveUP Media GmbH',
