3ef1bee35e506b62d13847ab61d5e3fc454e5f4d
Jonas Wolfram <jonas-wolfram@web.de>
2022-08-29 17:44:44 +0800

    Finished experience rework

--- src/app/data/jobs.ts
+++ src/app/data/jobs.ts
@@ -15,7 +15,7 @@ export const jobs: Job[] = [
   {
     title: 'Frontend developer',
     description:
-      'Adrodev is an agency for web and app development. During my employment there I was able to gain experience in the field of frontend development and managed to find out what it means to work in a small startup-like company.',
+      'Adrodev is an agency for web and app development. During my employment there I worked on numerous client projects using React and managed to find out what it means to work in a small startup-like company.',
     startDate: '2019',
     endDate: '2020',
     employer: {
@@ -26,12 +26,23 @@ export const jobs: Job[] = [
   {
     title: 'Frontend developer',
     description:
-      'Daimler Protics is a 100% subsidiary of the Daimler AG, which specialises in data processing. At Daimler Protics, I helped develop the frontend of an internal Daimler web application and was able to experience how work is done in a large company.',
+      'Daimler Protics is a 100% subsidiary of the Daimler AG, which specialises in data processing. At Daimler Protics, I helped develop the frontend of an internal Daimler web application with the help of the Angular framework.',
     startDate: '2020',
     endDate: '2020',
     employer: {
       name: 'Daimler Protics GmbH',
       logoUrl: '/assets/logos/daimler-protics-logo.png',
+    },
+  },
+  {
+    title: 'Frontend & Web3 Developer',
+    description:
+      'I built several web apps and dApps for clients coming from Europe and America. With an eye for design, the use of state of the art technology and years of experience I helped them modernise and scale their businesses.',
+    startDate: '2021',
+    endDate: 'now',
+    employer: {
+      name: 'Freelance',
+      logoUrl: '/assets/logos/portfolio-logo-bold-with-background.svg',
     },
   },
 ];
